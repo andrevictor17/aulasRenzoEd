aa28185bd4eb6553f6331e2cb43e27f89b9d8371
andrevictor17 <andre.victor.1996@gmail.com>
2016-04-13 10:09:03 -0300

    Update quick.py

--- quick.py
+++ quick.py
@@ -1,5 +1,6 @@
 import unittest
-def _quick_recursivo(seq):
+
+def quick_sort(seq):
     def sort(inicio, final):
         if inicio >= final:
             return seq
@@ -21,12 +22,6 @@ def _quick_recursivo(seq):
     sort(0, len(seq) - 1)
     return seq
 
-def quick_sort(seq):
-    '''
-    
-    '''
-    return _quick_recursivo(seq)
-
 
 class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_vazia(self):
@@ -40,7 +35,7 @@ class OrdenacaoTestes(unittest.TestCase):
 
     def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
-        
+
     def teste_lista_com_elementos_repetidos(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0, 9, 9]))
 
