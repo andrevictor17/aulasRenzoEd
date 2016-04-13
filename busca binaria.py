02ff08fb5a871f8c3caa3933eae7a97dc752ada5
andrevictor17 <andre.victor.1996@gmail.com>
2016-04-13 02:32:11 -0300

    Create busca binaria.py

---
+++ busca binaria.py
@@ -0,0 +1,52 @@
+import unittest
+def busca_binaria(seq, procurado):
+    """
+    Deve retornar o índice onde o elemento deveriar ser inserido em lista ordenada
+    :param procurado: elemento a ser procurado
+    :param seq: sequencia a ser pesquisada
+    :return: int
+    """
+    n = len(seq)
+    if n == 0:
+        return 0
+    direita = 0
+    esquerda = n-1
+
+    while direita <= esquerda:
+        meio = (direita + esquerda) // 2
+        if procurado < seq[meio]:
+            esquerda = meio-1
+        elif procurado > seq[meio]:
+            direita = meio+1
+        else:
+            return meio
+
+    return direita
+
+class BuscaBinariaTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertEqual(0, busca_binaria([], 1))
+        self.assertEqual(0, busca_binaria([], 2))
+        self.assertEqual(0, busca_binaria([], 3))
+
+    def teste_lista_unitaria(self):
+        self.assertEqual(0, busca_binaria([1], 0))
+        self.assertEqual(0, busca_binaria([1], 1))
+        self.assertEqual(1, busca_binaria([1], 2))
+        self.assertEqual(1, busca_binaria([1], 3))
+        self.assertEqual(1, busca_binaria([1], 4))
+
+    def teste_lista_nao_unitaria(self):
+        lista = list(range(10))
+        self.assertEqual(0, busca_binaria(lista, -2))
+        self.assertEqual(0, busca_binaria(lista, -1))
+        for i in lista:
+            self.assertEqual(i, busca_binaria(lista, i))
+        self.assertEqual(10, busca_binaria(lista, 10))
+        self.assertEqual(10, busca_binaria(lista, 11))
+        self.assertEqual(10, busca_binaria(lista, 12))
+
+    def teste_lista_elementos_repetidos(self):
+        lista = [1, 1, 1, 2, 2, 2]
+        self.assertEqual(0, busca_binaria(lista, 1))
+        self.assertEqual(3, busca_binaria(lista, 2))
